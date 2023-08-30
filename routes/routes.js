0f79be6c220f36a7c9effe6584440b9963bff5dc
Ulifly <ulifly@hotmail.com>
2023-08-29 22:28:25 -0600

    finished

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const UserModel = require('../model/UserModel');
 
-const userModel = ('../model/UserModel');
-
 const router = express.Router();
 
+// get all users
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await UserModel.find();
+    res.json(users);
+  } catch (err) {
+    console.log({ message: err });
+  }
+});
+
+// create a user
 router.post('/', (req, res) => {
   const user = new UserModel({
     title: req.body.title,
@@ -16,4 +26,37 @@ router.post('/', (req, res) => {
     .catch((err) => console.error(err, '☠️ error al guardar'));
 });
 
+// get one user
+router.get('/:userId', async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.params.userId);
+    res.json(user);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
 module.exports = router;
+
+// update the user
+
+router.patch('/:userId', async (req, res) => {
+  try {
+    const updateUser = await UserModel.updateOne(
+      { _id: req.params.userId },
+      { $set: { title: req.body.title, description: req.body.description } },
+    );
+    res.json(updateUser);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// delete the user
+router.delete('/:userId', async (req, res) => {
+  try {
+    const deleteUser = await UserModel.deleteOne({ _id: req.params.userId });
+    res.json(deleteUser);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
